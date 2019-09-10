42fbff155a5363e5854ae9dc37bf4ffb47a38812
stutommi <tommi.teetee@hotmail.com>
2019-09-10 16:07:48 +0300

    fix port

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
@@ -13,9 +14,19 @@ func about(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>About</h1>")
 }
 
+func getPort() string {
+	p := os.Getenv("PORT")
+	if p != "" {
+	return ":" + p
+	}
+	return ":3000"
+}
+
 func main() {
 	http.HandleFunc("/", index)
 	http.HandleFunc("/about", about)
 	fmt.Println("Server Starting...")
-	http.ListenAndServe(":3000", nil)
+	fmt.Println("Running in port", getPort())
+	http.ListenAndServe(getPort(), nil)
+
 }
